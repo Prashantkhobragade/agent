0e1baeda7d3640f537a5de038d9defbbd45ec4bb
Prashantkhobragade <prashantkhobragade011@gmail.com>
2024-07-22 17:07:38 +0530

    updated

--- output_json.py
+++ output_json.py
@@ -60,7 +60,7 @@ generate_json_task = Task(
     description='Process the provided {input_data} and generate a structured JSON output. Ensure that the output contains all necessary information and is formatted correctly. The response should only contains JSON',
     expected_output="A JSON object containing the processed data",
     agent=json_specialist,
-    output_json = 
+    #output_json = 
 )
 
 # Form the crew and kick off the process
