86f6f69e5c74f82dfd3fa29faf9a9f8a97ae8782
Prashantkhobragade <prashantkhobragade011@gmail.com>
2024-07-22 12:05:31 +0530

    updated

--- output_json.py
+++ output_json.py
@@ -1,6 +1,7 @@
 ##Agent
 from dotenv import load_dotenv
 import os
+import json
 
 from crewai import Agent, Task, Crew, Process
 from langchain_groq import ChatGroq
@@ -10,6 +11,12 @@ from datetime import datetime
 # Get the current timestamp
 timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
 
+my_response_template = {
+        "name": str,
+        "age": int,
+        "email": str
+
+}
 
 load_dotenv()
 
@@ -38,8 +45,35 @@ llm = ChatGroq(
     api_key = GROQ_API_KEY
 )
 
+input_data = input("Enter the data : ")
 
-#Tools
-serper_tool = SerperDevTool()
 
-topic:str = input("Enter the topic: ")
+# Define the JSON Specialist agent
+json_specialist = Agent(
+    role='JSON Specialist',
+    goal='To process {input_data} and provide output in JSON format.',
+    backstory='You have extensive experience in handling various data formats and are proficient in converting data into structured JSON objects. Your expertise ensures accurate and efficient JSON outputs.',
+    llm= llm,
+    response_template= my_response_template,
+    verbose=True
+)
+
+# Define the task for generating JSON output
+generate_json_task = Task(
+    description='Process the provided {input_data} and generate a structured JSON output. Ensure that the output contains all necessary information and is formatted correctly.',
+    expected_output="A JSON object containing the processed data",
+    agent=json_specialist,
+    OutputFormat = json
+)
+
+# Form the crew and kick off the process
+crew = Crew(
+    agents=[json_specialist],
+    tasks=[generate_json_task],
+    process=Process.sequential
+)
+
+# Kick off the crew
+result = crew.kickoff(inputs=input_data)
+print(result)
+
